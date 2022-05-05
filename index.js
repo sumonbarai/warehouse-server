55a51ea66c8e9594974987684964832f3a604b56
sumonbarai <sumonbarai78@gmail.com>
2022-05-05 21:23:57 +0600

    product get api create successfully

--- index.js
+++ index.js
@@ -3,25 +3,40 @@ const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
 require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 // middleware
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.spgtn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
-client.connect((err) => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  console.log("db connected");
-  client.close();
-});
 
+async function run() {
+  try {
+    await client.connect();
+    const productCollection = client
+      .db("productCollection")
+      .collection("product");
+
+    // all product get api
+    app.get("/product", async (req, res) => {
+      const query = {};
+      const cursor = productCollection.find(query);
+      const products = await cursor.toArray();
+      res.send(products);
+    });
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+// basic server set code
 app.get("/", (req, res) => {
   res.send("successfully server run");
 });
