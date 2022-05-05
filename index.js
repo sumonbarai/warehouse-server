6145fcc3fd7f21399f52eb4ad4bcedb2d691819b
sumonbarai <sumonbarai78@gmail.com>
2022-05-05 20:50:14 +0600

    server set up complete

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = process.env.PORT || 5000;
+require("dotenv").config();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.spgtn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+client.connect((err) => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  console.log("db connected");
+  client.close();
+});
+
+app.get("/", (req, res) => {
+  res.send("successfully server run");
+});
+
+app.listen(port, () => {
+  console.log(`my server is running ${port}`);
+});
